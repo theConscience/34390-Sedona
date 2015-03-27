a6c581b3a3858a0c439108052276f102d70fecd9
theConscience <vinton.mail@gmail.com>
2015-03-27 14:57:41 +0300

    исправил заамечаения по мобильному макету

--- sedona_feedback_page/js/script.js
+++ sedona_feedback_page/js/script.js
@@ -1,5 +1,25 @@
 (function() {
+    var plus = document.querySelectorAll('.counter-action.plus');
+    var minus = document.querySelectorAll('.counter-action.minus');
+    console.log(plus);
 
-  
+    [].forEach.call(plus, function(button){
+       button.addEventListener("click", function(event) {
+            event.preventDefault();
+            console.log('plus');
+            var val = this.parentNode.querySelector(".counter-input").value;
+            val = parseInt(val, 10);
+            this.parentNode.querySelector(".counter-input").value = val;
+            this.parentNode.querySelector(".counter-input").value += 1;
+            console.log(val);
+        });
+    });
 
+    [].forEach.call(minus, function(button) {
+        button.addEventListener("click", function(event) {
+            event.preventDefault();
+            console.log('plus');
+            this.parentNode.querySelector(".counter-input").value -= 1;
+        });
+    });
 })();
