e3603ee9530d8100b77907541ec383526c1e99e6
theConscience <vinton.mail@gmail.com>
2015-04-02 17:15:11 +0300

    убрал picture и sizes за ненадобностью

--- sedona_feedback_page/js/script.js
+++ sedona_feedback_page/js/script.js
@@ -1,16 +1,17 @@
 (function() {
     var plus = document.querySelectorAll('.counter-action.plus');
     var minus = document.querySelectorAll('.counter-action.minus');
-    console.log(plus);
+    var inputs = document.querySelectorAll('input[type=text]');
+    var adults = document.querySelector('.people-count');
+    var children = document.querySelector('.children-count');
+    console.log(adults, children);
 
     [].forEach.call(plus, function(button){
        button.addEventListener("click", function(event) {
             event.preventDefault();
             console.log('plus');
             var val = this.parentNode.querySelector(".counter-input").value;
-            val = parseInt(val, 10);
-            this.parentNode.querySelector(".counter-input").value = val;
-            this.parentNode.querySelector(".counter-input").value += 1;
+            this.parentNode.querySelector(".counter-input").value = parseInt(val) + 1;
             console.log(val);
         });
     });
@@ -19,7 +20,35 @@
         button.addEventListener("click", function(event) {
             event.preventDefault();
             console.log('plus');
-            this.parentNode.querySelector(".counter-input").value -= 1;
+            var val = this.parentNode.querySelector(".counter-input").value;
+            this.parentNode.querySelector(".counter-input").value = parseInt(val) - 1;
         });
     });
+
+    [].forEach.call(inputs, function(button) {
+        button.addEventListener("keyup", function(event) {
+            event.preventDefault();
+            console.log('change input');
+            var val = Number(this.value);
+            console.log(val);
+            console.log(typeof val);
+            if (isNaN(val)) {
+                console.log('please enter Number!');
+                this.value = '';
+            } else {
+                console.log('thank you!');
+            }
+        });
+    });
+
+    adults.addEventListener("change", function(event) {
+        event.preventDefault();
+        console.log('change adults count');
+    });
+
+    children.addEventListener("change", function(event) {
+        event.preventDefault();
+        console.log('change children count');
+    });
+
 })();
